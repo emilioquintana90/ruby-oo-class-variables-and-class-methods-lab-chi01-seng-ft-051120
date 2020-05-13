24d0532c7a6a5abfa36b9bb70633a785f2cea366
Emilio Quintana <emilioquintana90@gmail.com>
2020-05-13 13:40:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,19 @@ class Song
   @@count
   @@artists = []
   @@genres = []
+  @@genre_count = {}
   attr_accessor :name, :artist, :genre
   
-  def initialize
-    @name
-    @artist
-    @genre
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
     @@count++
     @@artists << artist
     @@genres << genre
+    
+    
   end
   
   def self.count
@@ -19,6 +23,10 @@ class Song
   
   def self.artists
     @@artists
+  end
+  
+  def self.genres
+    @@genres
   end
   
 end
